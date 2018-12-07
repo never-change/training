8057328b0058b85636f082a489b78ad5239e9ace
never-change <38969127+never-change@users.noreply.github.com>
2018-12-07 14:28:48 +0000

    init...

---
+++ pscloud_training/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'PS Cloud 练习',
+    'version': '12.0.1.0',
+    'summary': 'PS Cloud 练习',
+    'author': "www.mypscloud.com",
+    'website': 'https://www.mypscloud.com/',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+        #'security/ir.model.access.csv',
+        #'views/res_partner_views.xml',
+        #'views/training_views.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'js': [],
+    'css': [],
+    'auto_install': False,
+    'application': True,
+}
